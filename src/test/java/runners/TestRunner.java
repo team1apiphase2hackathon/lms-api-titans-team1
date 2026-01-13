3502576aa69d99bccdd40f43ff7742868459e473
mathumathi0989 <mathumathi.b@gmail.com>
2026-01-12 22:55:47 -0500

    user controller post scenarios and added helper class

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepDefinitions", "hooks"},
-        tags = "@skill",
+        tags = "@userController and @skill",
         plugin = {
                 "pretty",
                 "html:target/cucumber-report.html",
