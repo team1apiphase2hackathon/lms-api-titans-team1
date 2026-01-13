ad8d04a3e2959d1154287866fd964645a5d0dfe3
Santhana Mari Palani <santhanamari@Santhanas-MacBook-Air.local>
2026-01-13 17:47:59 -0600

    Json Schema validation added

--- src/test/java/utils/GlobalTestData.java
+++ src/test/java/utils/GlobalTestData.java
@@ -5,9 +5,9 @@ import java.util.List;
 
 public class GlobalTestData {
 	protected static List<Integer> batchIds = new ArrayList<>();
-	protected static int batchId;
-	protected static String batchName;
-	protected static int programId;
+	protected static int batchId = 70;
+	protected static String batchName = "Batch01";
+	protected static int programId = 70;
 	
 	public GlobalTestData() {
 		
