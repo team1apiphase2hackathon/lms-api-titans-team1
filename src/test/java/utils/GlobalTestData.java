707d8ecdf8302568cf01baa57e3b9d453591c10b
Thilagavathi Amulraj <Thilagavathi.Amulraj90@gmail.com>
2026-01-15 12:23:11 -0600

    commiting program module

--- src/test/java/utils/GlobalTestData.java
+++ src/test/java/utils/GlobalTestData.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class GlobalTestData {
 	protected static List<Integer> batchIds = new ArrayList<>();
 	protected static List<Integer> programIdList = new ArrayList<>();
-
+	protected static List<String> programNameList = new ArrayList<>();
 	protected static int batchId;
 	protected static String batchName;
 	protected static int programId;
